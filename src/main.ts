2a32bb9058fa80e7ba71df3245c8b30519c569c1
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-10-13 09:44:56 +0800

    🎉 init: project

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import './style.css'
+import 'uno.css'
+
+
+const btn = document.querySelector("#btn")!
+const audio = document.querySelector("#audio")! as HTMLAudioElement
+const animate_box = document.querySelector("#animate_box")!
+const count_box = document.querySelector("#count")! as HTMLDivElement
+
+
+let count = Number(localStorage.getItem("count") || 0)
+
+count_box.innerText = `公德数量: ${count}`
+
+btn?.addEventListener('click', () => {
+  // 播放声音
+  if (audio.paused) { audio.play() }
+  else { audio.currentTime = 0 }
+
+  // 公德加一动画
+  const animate = document.createElement("div")
+  animate.classList.add("animate")
+  animate.innerText = "公德 +1"
+  // 移除动画
+  setTimeout(() => animate.remove(), 780)
+  animate_box.appendChild(animate)
+
+
+  // 木鱼缩放动画
+  btn.classList.add('scale')
+  setTimeout(() => { btn.classList.remove('scale') }, 90)
+
+  // 公德数量加一
+  count++
+  count_box.innerText = `公德数量: ${count}`
+  localStorage.setItem("count", count + "")
+
+})
